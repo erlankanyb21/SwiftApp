1094fa0246b2d9303588f42973514110b4866dc8
erlankanybekov <kanybekov668@gmail.com>
2024-10-15 22:22:54 +0600

    Initial Commit

---
+++ SwiftAppTests/SwiftAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppTests.swift
+//  SwiftAppTests
+//
+//  Created by User on 10/15/24.
+//
+
+import Testing
+@testable import SwiftApp
+
+struct SwiftAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
